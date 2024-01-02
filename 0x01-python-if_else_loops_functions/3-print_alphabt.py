54d5e53dd0d04f6d943bd74b82265a0bb901b3c8
Hector Macassa <cbmacassa@gmail.com>
2024-01-02 15:52:12 -0800

    ASCII

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for alphabet in range(97, 123):
+    if alphabet == 101:
+        continue;
+    elif alphabet == 113:
+        continue;
+    else:
+        print("{}".format(chr(alphabet)), end="")
