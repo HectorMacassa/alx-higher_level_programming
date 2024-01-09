34f12d3105bf5e5583a6e7d5117afc0c27b8431f
Hector Macassa <cbmacassa@gmail.com>
2024-01-09 02:31:28 -0800

    Delete at

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    i = 0
+    while i < len(my_list):
+        if i % 2 == 0:
+            return True
+        else: 
+            return False
+    i += 1
