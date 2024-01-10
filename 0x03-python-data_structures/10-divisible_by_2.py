fbc25a9dc9bd8e10cf3f2f58ebfc4a5c3a934063
Hector Macassa <cbmacassa@gmail.com>
2024-01-09 18:48:33 -0800

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-   multiples_of_2 = []
+    if my_list is None:
+        my_list = []
 
-   for i in range(len(my_list)):
-       if my_list[i] % 2 == 0:
-           multiples_of_2.append(True)
-       else:
-           multiples_of_2.append(False)
-   return multiples_of_2
+    multiples_of_2 = [num % 2 == 0 for num in my_list]
+    return multiples_of_2
