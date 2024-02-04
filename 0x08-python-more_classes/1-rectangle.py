23b01089175f57b8e508466d051d20cbe6153876
Hector Macassa <cbmacassa@gmail.com>
2024-02-04 10:14:10 -0800

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,7 @@
 This module defines a rectangle class
 """
 
+
 class Rectangle:
     """
     This class represents a rectangle
@@ -47,4 +48,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
