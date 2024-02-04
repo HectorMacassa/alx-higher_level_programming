8f3df3c16bb3f9a13cc9a5216f14da48d05416c8
Hector Macassa <cbmacassa@gmail.com>
2024-02-04 10:28:50 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a rectangle"""
 
+
 class Rectangle:
     """
     This class represents a rectangle
@@ -10,7 +11,7 @@ class Rectangle:
     """
 
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """
         Initializes a new rectangle
