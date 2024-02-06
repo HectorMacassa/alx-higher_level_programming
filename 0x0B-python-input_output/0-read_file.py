59fd6aded6cd6adcaabc70952854f56ef5597b99
Hector Macassa <cbmacassa@gmail.com>
2024-02-06 04:57:31 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a function that reads a text file"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a file and prints it to stdout
+
+    Args:
+        filename (str): File to be read
+    """
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read())
