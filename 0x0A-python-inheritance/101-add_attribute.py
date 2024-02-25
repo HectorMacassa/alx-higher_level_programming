b8e0665a8a5b8e83bc7828f04ca79989d583946c
Hector Macassa <cbmacassa@gmail.com>
2024-02-25 12:19:17 -0800

    Can I

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a class add_attribute"""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """
+    Adds anew attribute to an object if possible, otherwise raises a TypeError
+
+    Args:
+        obj: The object to which the attribute will be added
+        attr_name: The name of the attribute to be added
+        attr_value: The value of the attribute to be added
+
+    Raises:
+        TypeError: If the object cannot have a new attribute added
+    """
+    if hasattr(obj, '__dict__'):
+        obj.__dict__[attr_name] = attr_value
+    elif hasattr(type(obj), '__dict__'):
+        type(obj).__dict__[attr_name] = attr_value
+    else:
+        raise TypeError("can't add new attribute")
