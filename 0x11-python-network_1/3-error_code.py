d8223f5da241eb34c0ec10c6db66020b0b41da63
Hector Macassa <cbmacassa@gmail.com>
2024-04-30 17:21:14 -0700

    What's my status? #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Takes in a URL, sends a request to the URL and displays the body of the response
+Takes in a URL, sends a request to the URL and displays the body of theresponse
 """
 
 import urllib.request
