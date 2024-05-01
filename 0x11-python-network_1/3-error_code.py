e3d09e86144234632d8f37ecfb6b72fa2724367d
Hector Macassa <cbmacassa@gmail.com>
2024-04-30 17:13:48 -0700

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Takes in a URL, sends a request to the URL and displays the body of the response
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode("utf-8")
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"An error occurred: {e.reason}")
