f42481ef4d5e2225d336e5440a6b4936e051e121
Hector Macassa <cbmacassa@gmail.com>
2024-05-22 19:03:29 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function getStatusCode(url) {
+  request.get(url, (error, response) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`code: ${response.statusCode}`);
+    }
+  });
+}
+
+if (process.argv.length < 3) {
+  console.error('Please provide a URL as an argument.');
+} else {
+  const url = process.argv[2];
+  getStatusCode(url);
+}
