f4704510d505bf7e256777ac5035b1f22ded2a3e
Hector Macassa <cbmacassa@gmail.com>
2024-12-21 08:09:55 -0800

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,6 +4,10 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
 }
 
 module.exports = Rectangle;
